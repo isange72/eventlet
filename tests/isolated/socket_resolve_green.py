12cc08de1a27ee39cc8583b7186cdc2078e35421
Sergey Shepelev <temotor@gmail.com>
2017-01-17 15:10:14 +0500

    tests: more concurrency in socket_resolve_green
    Hope it would fix Travis failures.

--- tests/isolated/socket_resolve_green.py
+++ tests/isolated/socket_resolve_green.py
@@ -8,18 +8,18 @@ if __name__ == '__main__':
     import dns.message
     import dns.query
 
+    n = 10
+    delay = 0.01
+    addr_map = {'eventlet-test-host{0}.'.format(i): '0.0.0.{0}'.format(i) for i in range(n)}
+
     def slow_udp(q, *a, **kw):
-        addr = '0.0.0.1'
-        if 'host2' in str(q.question):
-            addr = '0.0.0.2'
-        if 'host3' in str(q.question):
-            addr = '0.0.0.3'
+        addr = addr_map[q.question[0].name.to_text()]
         r = dns.message.make_response(q)
         r.index = None
         r.flags = 256
         r.answer.append(dns.rrset.from_text(str(q.question[0].name), 60, 'IN', 'A', addr))
         r.time = 0.001
-        eventlet.sleep(0.1)
+        eventlet.sleep(delay)
         return r
 
     dns.query.udp = slow_udp
@@ -31,15 +31,12 @@ if __name__ == '__main__':
         except socket.error as e:
             print('name: {0} error: {1}'.format(name, e))
 
-    pool = eventlet.GreenPool()
+    pool = eventlet.GreenPool(size=n + 1)
     t1 = time.time()
-    pool.spawn(fun, 'eventlet-test-host1.')
-    pool.spawn(fun, 'eventlet-test-host2.')
-    pool.spawn(fun, 'eventlet-test-host3.')
+    for name in addr_map:
+        pool.spawn(fun, name)
     pool.waitall()
     td = time.time() - t1
-    assert 0.1 <= td < 0.3, 'Resolve time expected: ~0.1s, real: {0:.3f}'.format(td)
-    assert results.get('eventlet-test-host1.') == '0.0.0.1'
-    assert results.get('eventlet-test-host2.') == '0.0.0.2'
-    assert results.get('eventlet-test-host3.') == '0.0.0.3'
+    assert delay <= td < delay * n, 'Resolve time expected: ~{0}s, real: {1:.3f}'.format(delay, td)
+    assert sorted(addr_map.items()) == sorted(results.items())
     print('pass')
